0440b4a19760d9f869775c706d8a8ba0a3784065
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-02-16 20:29:10 +0100

    components setup

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import logo from "../assets/images/logo.svg";
 
 function Nav() {
-  return <div>Nav</div>;
+  return (
+    <nav className="nav">
+      <div className="nav-logo">
+        <img src={logo} alt="Logo" />
+      </div>
+      <div className="nav-routes">
+        <ul>
+          <li>Features</li>
+          <li>Pricing</li>
+          <li>Resourcees</li>
+        </ul>
+        <div className="nav-login">
+          <li>Login</li>
+          <button>Sign up</button>
+        </div>
+      </div>
+      <i className="fa-solid fa-bars"></i>
+      <i className="fa-solid fa-xmark"></i>
+    </nav>
+  );
 }
 
 export default Nav;
