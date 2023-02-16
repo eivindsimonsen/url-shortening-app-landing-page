51dfc4800bb872ed11e1a73da2f6770b45698fef
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-02-16 20:28:57 +0100

    Moved image folder to asset folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import "./sass/style.scss";
 import Nav from "./components/Nav";
+import Hero from "./components/Hero";
+import Shortener from "./components/Shortener";
+import Stats from "./components/Stats";
+import Banner from "./components/Banner";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="container">
+    <div className="app container">
       <Nav />
+      <Hero />
+      <Shortener />
+      <Stats />
+      <Banner />
+      <Footer />
     </div>
   );
 }
