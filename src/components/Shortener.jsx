5ab1c4d363f24d8b201738f8f4e4206ef9f7529d
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-02-19 18:45:20 +0100

    Added error code, not yet hidden

--- src/components/Shortener.jsx
+++ src/components/Shortener.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import bgImage from "../assets/images/bg-shorten-mobile.svg";
+import bgImageDesktop from "../assets/images/bg-shorten-desktop.svg";
 
 function Shortener() {
   return (
-    <section className="shortener-container">
-      <input type="text" placeholder="Shorten a link here..." />
-      <button className="cta">Shorten it!</button>
-    </section>
+    <div className="shortener">
+      <section className="shortener-container">
+        <input className="input-error" type="text" placeholder="Shorten a link here..." />
+        <div className="input-error-message">Please add a link</div>
+        <button className="cta">Shorten it!</button>
+        <img className="shortener-container-bg-image-mobile" src={bgImage} alt="" />
+        <img className="shortener-container-bg-image-desktop" src={bgImageDesktop} alt="" />
+      </section>
+      <section className="output-links">
+        <div>https://megalinkadded.com</div>
+        <hr />
+        <div>https://newmegalinkgiven</div>
+        <button className="cta">Copy</button>
+      </section>
+    </div>
   );
 }
 
